b88e20b0698074541c7b4b5de6862a8213482336
chy202 <sepma017@gmail.com>
2021-12-15 19:02:38 +0900

    조현영, 강지원 프로젝트
    2021년 12월 15일 프로젝트를 FBV로 만들었습니다.

--- friend/models.py
+++ friend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Member(models.Model):
+    name = models.CharField(max_length=10)
+    age = models.CharField(max_length=5)
+    birth = models.DateTimeField()
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name} ( {self.email} )'
+
+    def get_absolute_url(self):
+        return f'/friend/{self.pk}/'
--- friend/views.py
+++ friend/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
+def index(request):
+    members = Member.objects.all()
+
+    return render(
+        request,
+        'friend/index.html',
+        {
+            'members': members,
+        }
+    )
+
+def single_member_page(request, pk):
+    member = Member.objects.get(pk=pk)
+
+    return render(
+        request,
+        'friend/single_member_page.html',
+        {
+            'member': member,
+        }
+    )
